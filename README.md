6c895fdd7dba2c507eabc54723641ea3fc6380b8
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-10 01:15:17 +0200

    added few more details

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Install Dependency
+```sh
+pip3 install -r requirment.txt
+```
+
+### Run Application
+```sh
+flask --app  file-processor.py run
+```
--- file-processor.py
+++ file-processor.py
@@ -1,12 +1,24 @@
-filename = 'sample.txt'
-lines = words = letters = 0
+from flask import Flask
 
-with open(filename, 'r') as file:
-    for line in file:
-        lines += 1
-        words += len(line.split())
-        letters += sum(c.isalpha() for c in line)
+app = Flask(__name__)
 
-print(f'Lines: {lines}')
-print(f'Words: {words}')
-print(f'Letters: {letters}')
+@app.route("/")
+def hello_world():
+    return "PONG"
+
+@app.route("/details")
+def file_processor():
+    filename = 'requirment.txt'
+    lines = words = letters = 0
+
+    with open(filename, 'r') as file:
+        for line in file:
+            lines += 1
+            words += len(line.split())
+            letters += sum(c.isalpha() for c in line)
+
+    print(f'Lines: {lines}')
+    print(f'Words: {words}')
+    print(f'Letters: {letters}')
+
+    return "PROCESSED"
