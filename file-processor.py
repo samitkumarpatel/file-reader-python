96cf07b0c3d25f5943c9646028e34be54d545a81
samitkumarpatel <samitkumarpatel@gmail.com>
2024-05-28 23:53:15 +0200

    rabbitmq to redis pubsub switch

--- file-processor.py
+++ file-processor.py
@@ -1,4 +1,16 @@
 from flask import Flask
+import redis
+
+
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+channel = 'channel'
+pubsub = redis_client.pubsub()
+pubsub.subscribe(channel)
+print(f"Subscribed to {channel}. Waiting for messages...")
+for message in pubsub.listen():
+    if message['type'] == 'message':
+        print(f"Received: {message['data'].decode('utf-8')}")
+
 
 app = Flask(__name__)
 
